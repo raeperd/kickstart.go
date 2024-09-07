82154c402d2b1760eb0285032a8a64f7d363e0d2
raeperd <raeperd117@gmail.com>
2024-09-08 01:05:13 +0900

    feat: Add middleware to handle recover from panic

--- route.go
+++ route.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
+	"runtime"
 	"time"
 )
 
@@ -10,7 +12,10 @@ func route() http.Handler {
 	mux := http.NewServeMux()
 	mux.Handle("/health", handleGetHealth())
 	mux.Handle("/debug/", handleGetDebug())
-	return accesslog(mux)
+
+	handler := accesslog(mux)
+	handler = recovery(handler)
+	return handler
 }
 
 func accesslog(next http.Handler) http.Handler {
@@ -28,6 +33,36 @@ func accesslog(next http.Handler) http.Handler {
 			slog.String("ip", r.RemoteAddr),
 			slog.Int("status", wm.status),
 			slog.Int("bytes", wm.numBytes))
+	})
+}
+
+func recovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		wm := responseMirror{ResponseWriter: w}
+		defer func() {
+			if err := recover(); err != nil {
+				if err == http.ErrAbortHandler {
+					// Handle the abort gracefully
+					return
+				}
+
+				stack := make([]byte, 1024)
+				n := runtime.Stack(stack, true)
+
+				slog.ErrorContext(r.Context(), "panic!",
+					slog.Any("error", err),
+					slog.String("stack", string(stack[:n])),
+					slog.String("method", r.Method),
+					slog.String("path", r.URL.Path),
+					slog.String("query", r.URL.RawQuery),
+					slog.String("ip", r.RemoteAddr))
+
+				if !wm.written() {
+					http.Error(w, fmt.Sprintf("%v", err), 500)
+				}
+			}
+		}()
+		next.ServeHTTP(&wm, r)
 	})
 }
 
@@ -49,4 +84,8 @@ func (re *responseMirror) Write(b []byte) (int, error) {
 func (re *responseMirror) WriteHeader(statusCode int) {
 	re.status = statusCode
 	re.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (re responseMirror) written() bool {
+	return re.status != 0
 }
